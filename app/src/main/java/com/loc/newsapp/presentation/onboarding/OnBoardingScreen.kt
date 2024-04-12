5c1aab227a146c3ab086c165aa2393b57aa22256
Jorge Flores Carlos <florescarlos167@gmail.com>
2024-04-12 17:59:51 -0600

    implement first models

--- app/src/main/java/com/loc/newsapp/presentation/onboarding/OnBoardingScreen.kt
+++ app/src/main/java/com/loc/newsapp/presentation/onboarding/OnBoardingScreen.kt
@@ -1,7 +1,7 @@
 package com.loc.newsapp.presentation.onboarding
 
 import androidx.compose.foundation.ExperimentalFoundationApi
-import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -9,19 +9,26 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import com.loc.newsapp.presentation.Dimension.MediumPadding2
+import com.loc.newsapp.presentation.Dimension.PageIndicatorWidth
+import com.loc.newsapp.presentation.common.NewsButton
+import com.loc.newsapp.presentation.common.NewsTextButton
 import com.loc.newsapp.presentation.onboarding.components.OnboardingPage
+import com.loc.newsapp.presentation.onboarding.components.PageIndicator
+import kotlinx.coroutines.launch
 
 @OptIn(ExperimentalFoundationApi::class)
 @Composable
-fun OnBoardingScreen() {
+fun OnBoardingScreen(event: (OnBoardingEvent) -> Unit) {
     Column(modifier = Modifier.fillMaxSize()) {
         val pagerState = rememberPagerState(initialPage = 0) {
             pages.size
@@ -45,9 +52,45 @@ fun OnBoardingScreen() {
             modifier = Modifier
                 .fillMaxWidth()
                 .padding(horizontal = MediumPadding2)
-                .navigationBarsPadding()
+                .navigationBarsPadding(),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
         ) {
+            PageIndicator(
+                modifier = Modifier.width(PageIndicatorWidth),
+                pageSize = pages.size,
+                selectedPage = pagerState.currentPage
+            )
 
+            Row(verticalAlignment = Alignment.CenterVertically) {
+
+                val scope = rememberCoroutineScope()
+
+                if (buttonState.value[0].isNotEmpty()) {
+                    NewsTextButton(
+                        text = buttonState.value[0],
+                        onClick = {
+                            scope.launch {
+                                pagerState.animateScrollToPage(page = pagerState.currentPage - 1)
+                            }
+                        }
+                    )
+                }
+
+                NewsButton(
+                    text = buttonState.value[1],
+                    onClick = {
+                        scope.launch {
+                            if (pagerState.currentPage == 2) {
+                               event(OnBoardingEvent.SaveAppEntry)
+                            } else {
+                                pagerState.animateScrollToPage(page = pagerState.currentPage + 1)
+                            }
+                        }
+                    }
+                )
+            }
         }
+        Spacer(modifier = Modifier.weight(0.5f))
     }
 }
